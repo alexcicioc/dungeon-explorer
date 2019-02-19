39bb2081f2feface439fede3265b1513557a3b02
Andrei Verdes <andrew.xd23@gmail.com>
2019-02-19 23:03:10 +0200

    Add map generation functions

--- game.js
+++ game.js
@@ -1,4 +1,66 @@
+const api = 'https://api.myjson.com/bins/gfi3u';
+
 let animationLock = false;
+
+init();
+
+function init(){
+	fetchMap().then(function(mapJson){
+		renderMap(mapJson);
+	});
+}
+
+function fetchMap(){
+	return new Promise(function(resolve, reject){
+		$.get('https://api.myjson.com/bins/gfi3u', function(mapJson){
+			resolve(mapJson);
+		})
+		.fail(function(){
+			reject();
+		});
+	});
+}
+
+function renderMap(mapJson){
+	renderTopWall();
+	renderTiles(mapJson);
+	renderBottomWall();
+}
+
+function renderTiles(mapJson){
+	mapJson.forEach(function(rowJson){
+		let rowElement = $('<div></div>');
+		rowElement.addClass('map-row');
+		
+		rowJson.forEach(function(tileJson){
+			let tileElement = $('<div></div>');
+			tileElement.addClass('tile');
+			tileElement.addClass(tileJson);
+			rowElement.append(tileElement);
+		});
+		
+		$('#map').append(rowElement);
+	});
+	
+}
+
+function renderTopWall(){
+	let topWallElement = $('<div></div>');
+	topWallElement.addClass('wall-top');
+	
+	let entranceElement = $('<div></div>');
+	entranceElement.addClass('entrance');
+	topWallElement.append(entranceElement);
+	
+	$('#map').append(topWallElement);
+}
+
+function renderBottomWall(){
+	let bottomWallElement = $('<div></div>');
+	bottomWallElement.addClass('wall-bottom');
+	
+	$('#map').append(bottomWallElement);
+}
 
 $(document).keydown(function (event) {
     if (animationLock) {
