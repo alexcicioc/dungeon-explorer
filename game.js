badb5843e66b6e6d45402efdaa9b3382203251cd
Andrei R. Verdes <andrei.r.verdes@kp.org>
2019-02-20 10:51:54 +0200

    Remove usage of promise Move generation of static html from js directly into the html file.

--- game.js
+++ game.js
@@ -4,62 +4,35 @@ let animationLock = false;
 
 init();
 
-function init(){
-	fetchMap().then(function(mapJson){
-		renderMap(mapJson);
-	});
+function init() {
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: api,
+        success: function (mapJson) {
+            renderMap(mapJson)
+        }
+    });
 }
 
-function fetchMap(){
-	return new Promise(function(resolve, reject){
-		$.get('https://api.myjson.com/bins/gfi3u', function(mapJson){
-			resolve(mapJson);
-		})
-		.fail(function(){
-			reject();
-		});
-	});
+function renderMap(mapJson) {
+    renderTiles(mapJson);
 }
 
-function renderMap(mapJson){
-	renderTopWall();
-	renderTiles(mapJson);
-	renderBottomWall();
-}
+function renderTiles(mapJson) {
+    mapJson.forEach(function (rowJson) {
+        let rowElement = $('<div></div>');
+        rowElement.addClass('map-row');
 
-function renderTiles(mapJson){
-	mapJson.forEach(function(rowJson){
-		let rowElement = $('<div></div>');
-		rowElement.addClass('map-row');
-		
-		rowJson.forEach(function(tileJson){
-			let tileElement = $('<div></div>');
-			tileElement.addClass('tile');
-			tileElement.addClass(tileJson);
-			rowElement.append(tileElement);
-		});
-		
-		$('#map').append(rowElement);
-	});
-	
-}
+        rowJson.forEach(function (tileJson) {
+            let tileElement = $('<div></div>');
+            tileElement.addClass('tile');
+            tileElement.addClass(tileJson);
+            rowElement.append(tileElement);
+        });
 
-function renderTopWall(){
-	let topWallElement = $('<div></div>');
-	topWallElement.addClass('wall-top');
-	
-	let entranceElement = $('<div></div>');
-	entranceElement.addClass('entrance');
-	topWallElement.append(entranceElement);
-	
-	$('#map').append(topWallElement);
-}
-
-function renderBottomWall(){
-	let bottomWallElement = $('<div></div>');
-	bottomWallElement.addClass('wall-bottom');
-	
-	$('#map').append(bottomWallElement);
+        $('#map').append(rowElement);
+    });
 }
 
 $(document).keydown(function (event) {
@@ -69,27 +42,28 @@ $(document).keydown(function (event) {
     switch (event.which) {
         case 37:
             //left
-            return go({ left: '-=32' });
+            return go({left: '-=32'});
 
         case 38:
             // up
-            return go({ top: '-=32' });
+            return go({top: '-=32'});
 
         case 39:
             //right
-            return go({ left: '+=32' });
+            return go({left: '+=32'});
 
         case 40:
             //down
-            return go({ top: '+=32' });
+            return go({top: '+=32'});
 
-        default: return; // exit this handler for other keys
+        default:
+            return; // exit this handler for other keys
     }
 });
 
 function go(direction) {
     animationLock = true;
-    // TODO change adventurer default css animation to a move animation
+
     $('#adventurer').animate(direction, 500, function () {
         animationLock = false;
     });
